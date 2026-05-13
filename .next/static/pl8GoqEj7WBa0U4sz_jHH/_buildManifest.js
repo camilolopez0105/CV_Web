0f17df5a59836011bd94b51b0c20c33dce1077cc
camilolopez0105 <camilo.lopez0105@gmail.com>
2026-05-14 00:47:56 +0200

    Changes

--- .next/static/pl8GoqEj7WBa0U4sz_jHH/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/1b7cc6686177d513.css","static/chunks/pages/index-7710ae1c7fc1bc3a.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
