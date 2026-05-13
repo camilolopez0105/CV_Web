5ca15a9a767e8847cf49cd4e101516e5d4ea9830
camilolopez0105 <camilo.lopez0105@gmail.com>
2026-05-13 23:55:28 +0200

    style changes

---
+++ out/_next/static/mylwxv6nI_EZfYdpC2S4w/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/1b7cc6686177d513.css","static/chunks/pages/index-823dad8ba60b4703.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
