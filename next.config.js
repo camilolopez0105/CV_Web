885172ce57239590293b7fb6a93828d6674fb7d4
camilolopez0105 <camilo.lopez0105@gmail.com>
2026-05-14 00:04:13 +0200

    changes

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
-const isProduction = process.env.NODE_ENV === 'production'
-
 const nextConfig = {
-  output: isProduction ? 'export' : undefined,
-  basePath: isProduction ? '/CV_Web' : '',
+  output: 'export',
+  basePath: '/CV_Web',
   images: {
     unoptimized: true,
   },
-  trailingSlash: isProduction,
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
