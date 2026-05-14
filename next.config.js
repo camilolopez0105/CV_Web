a4b9f75fe0f05b426dd382d6912fa4b371d45dc9
camilolopez0105 <camilo.lopez0105@gmail.com>
2026-05-14 12:15:55 +0200

    Fix config for local dev: remove basePath, fix image path

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/CV_Web',
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
 }
 
 module.exports = nextConfig
